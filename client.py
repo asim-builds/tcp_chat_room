ce826e801f7111854c3a62ecbbc05cb91e8d5b95
asim-builds <asim.baral2223@gmail.com>
2025-05-21 07:41:12 +0545

    Basic Completed: Clients can now send message to each other

--- client.py
+++ client.py
@@ -1,11 +1,28 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 65432        # The same port as the server
 
+def receive_messages(sock):
+    while True:
+        try:
+            msg = sock.recv(1024)  # Buffer size is 1024 bytes
+            if not msg:
+                print("Disconnected from server.")
+                break  # No message means the server has closed the connection
+            print("\rFriend: " + msg.decode() + "\nYou: ", end="")
+        except ConnectionResetError:
+            print("Connection reset by server")
+            break
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print(f"Connected to server at {HOST}:{PORT}")
+
+    # Start a thread to receive messages
+    threading.Thread(target=receive_messages, args=(s,), daemon=True).start()
+    
     while True:
         message = input("You: ")
         if message.lower() == 'exit':
